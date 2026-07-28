16c25086bf57db7700493da3ccbf144675ff4e75
AaronAngat <aaronangat30@gmail.com>
2026-07-28 22:43:14 +0800

    added postgre db connection from supabase

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { pool } from "./db.js";
 
 dotenv.config();
 
@@ -10,6 +11,15 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({message: "BudolRide Navigator backend is running"});
+});
+
+app.get("/db-test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({ success: true, time: result.rows[0]});
+    }catch (err){
+        res.status(500).json({ sucesss: false, error: (err as Error).message });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
